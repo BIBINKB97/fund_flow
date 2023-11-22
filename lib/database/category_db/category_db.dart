8a0273982c8a88d6bcbd7a1d27bfe1d13a3b5b2a
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-22 23:35:37 +0530

    changed colors

--- lib/database/category_db/category_db.dart
+++ lib/database/category_db/category_db.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fund_flow/model/category_model/category_model.dart';
--- lib/database/chart_db/chart_db.dart
+++ lib/database/chart_db/chart_db.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
+
 import 'package:flutter/material.dart';
 import 'package:fund_flow/database/transaction_db/transaction_db.dart';
 import 'package:fund_flow/model/category_model/category_model.dart';

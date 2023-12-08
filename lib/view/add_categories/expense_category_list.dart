23e73a239ae223bc00e637b40fb1a19e971fee4f
bibin k b <bibinkochumalayil007@gmail.com>
2023-12-08 23:04:21 +0530

    added alert box for expense category list

--- lib/view/add_categories/expense_category_list.dart
+++ lib/view/add_categories/expense_category_list.dart
@@ -33,7 +33,55 @@ class ExpenseCategoryList extends StatelessWidget {
                           ),
                           IconButton(
                               onPressed: () {
-                               
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: Text(
+                                          'Alert !',
+                                          style: TextStyle(
+                                              color: kred,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                        content: Text(
+                                          'Do you want to delete this category ?',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text(
+                                                'No',
+                                                style: TextStyle(
+                                                    color: kblue,
+                                                    fontSize: 19,
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              )),
+                                          TextButton(
+                                              onPressed: () {
+                                                CategoryDB.instance
+                                                    .deleteCategory(
+                                                        category.id);
+
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text(
+                                                'Yes',
+                                                style: TextStyle(
+                                                    color: kred,
+                                                    fontSize: 19,
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              ))
+                                        ],
+                                      );
+                                    });
                               },
                               icon: CircleAvatar(
                                 backgroundColor: kblack54,
@@ -46,8 +94,11 @@ class ExpenseCategoryList extends StatelessWidget {
                   );
                 },
                 separatorBuilder: (context, index) {
-                  return SizedBox(
-                    height: height * 0.002,
+                  return Divider(
+                    thickness: 1,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.white54,
                   );
                 },
                 itemCount: newList.length,
